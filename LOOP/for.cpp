c0031daaf793f18410a833fa8a6e0020f0f89020
PARTHIVzala <166072111+PARTHIVzala@users.noreply.github.com>
2025-06-04 10:09:27 +0530

    loop

---
+++ LOOP/for.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"i=a";
+	cout<<"odd no"<<"i<=5";
+	print<<"i++";
+}
---
+++ LOOP/rate of interest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"rate of interest";
+	cout<< "rate of interest"<< "p*r*t/100";
+	int p=100 , r=10 , t=2 ;
+	float ans = (100*10*2/100);
+	cout<< ans;
+}
+
