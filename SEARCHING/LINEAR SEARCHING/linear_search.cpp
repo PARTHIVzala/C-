25e6ad1e975ea0a62c415d815c5722dfb4091d03
PARTHIVzala <166072111+PARTHIVzala@users.noreply.github.com>
2025-07-23 23:26:38 +0530

    SEARCHING

---
+++ SEARCHING/LINEAR SEARCHING/linear_search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int linearSearch(int arr[], int n, int target) {
+	for (int i = 0; i < n; i++) {
+			if (arr[i] == target) {
+			return i;
+		}
+	}
+	return -1;
+	}
+int main() {
+		int arr[] = {5, 3, 8, 2, 9};
+		int target = 8;
+		int n = sizeof(arr) / sizeof(arr[0]);
+		int result = linearSearch(arr, n, target);
+		if (result != -1)
+			cout << "Element found at index: " << result << endl;
+		else
+			cout << "Element not found." << endl;
+			return 0;
+}
